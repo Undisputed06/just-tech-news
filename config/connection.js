c13ef4f84c2dbdb81077fcccdda534dd093763ae
Gary <52983305+Undisputed06@users.noreply.github.com>
2022-06-18 14:45:38 -0500

    homepage-view

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
-//import the Sequalize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connecton to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-})
+  });
+}
 
 module.exports = sequelize;
